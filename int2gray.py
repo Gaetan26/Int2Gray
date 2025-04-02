0160b3f0cb4b5febb78a61bda2c95ff569d9fedd
Gaetan <gaetansg26@gmail.com>
2025-04-03 00:31:01 +0100

    Code fait et organiser en functions dans le fichier int2gray.py

--- int2gray.py
+++ int2gray.py
@@ -2,9 +2,10 @@
 
 import numpy as np
 from PIL import Image
-from time import strftime
 
 
+#
+#
 def extractImageSize(fileinput):
     rows = int(fileinput.__iter__().__next__())
     cols = int(fileinput.__iter__().__next__())
@@ -12,37 +13,44 @@ def extractImageSize(fileinput):
     return (rows, cols)
 
 
+#
+#
 def createMatrix(rows, cols):
     return np.zeros((rows, cols))
 
 
+#
+#
 def fillMatrix(fileinput, matrix):
     rows, cols = matrix.shape
     
     for y in range(rows):
         row = fileinput.__iter__().__next__()
-        row = row.rstip(" ").lstrip(" ").split(" ")
+        row = row.rstrip(" ").lstrip(" ").split(" ")
         
         for x in range(cols):
-            matrix[y, x] = int( row[x] )
+            matrix[y, x] = int(row[x])
     
     return True
 
 
+#
+#
 def findMinMaxValues(matrix):
     rows, cols = matrix.shape
     minval, maxval = matrix[0, 0], matrix[0, 0]
     
     for y in range(rows):
-        
         for x in range(cols):
             value = matrix[y, x]
-            minval = value if minval < value else minval 
-            maxval = value if maxval > value else minval 
+            minval = value if value < minval else minval 
+            maxval = value if value > maxval else maxval 
     
     return minval, maxval
 
 
+#
+#
 def findParamatersOfLinearEquation(minval, maxval):
     m = 255 / (maxval - minval)
     b = -minval * m
@@ -50,6 +58,8 @@ def findParamatersOfLinearEquation(minval, maxval):
     return m, b
 
 
+#
+#
 def createGrayscaleImage(rows, cols):
     image = Image.new('L', (cols, rows))
     pixels = image.load()
@@ -57,6 +67,8 @@ def createGrayscaleImage(rows, cols):
     return image, pixels
 
 
+#
+#
 def matchMatrixValuesWithPixels(matrix, pixels, m, b):
     rows, cols = matrix.shape
     for y in range(rows):
@@ -67,6 +79,42 @@ def matchMatrixValuesWithPixels(matrix, pixels, m, b):
     return True
 
 
-def saveImage(image):
-    image.save(strftime("%c")+".png", "PNG")
+#
+#
+def saveImage(image, name, format_):
+    image.save(f"{name}.{format_}", format_)
     return True
+
+
+#
+#
+def process(fileinput, output, format_):
+    
+    # ouvrir le fichier avec les entrees
+    fileinput = open(fileinput, "r")
+    
+    # extraires la taille de l'image + creer la matrice
+    rows, cols = extractImageSize(fileinput)
+    matrix = createMatrix(rows=rows, cols=cols)
+    
+    # remplir la matrice avec les valeurs d'entrees
+    fillMatrix(fileinput=fileinput, matrix=matrix)
+    
+    # fermer le fichier des entrees
+    fileinput.close()
+
+    # trouver la valeur MINIMAL & MAXIMAL
+    minval, maxval = findMinMaxValues(matrix)
+
+    # trouver les parametres de l'equation lineaire
+    m, b = findParamatersOfLinearEquation(minval=minval, maxval=maxval)
+
+    # creer une image en GRAYSCALE
+    image, pixels = createGrayscaleImage(rows=rows, cols=cols)
+    matchMatrixValuesWithPixels(matrix=matrix, pixels=pixels, m=m, b=b)
+    
+    # sauvegarder l'image
+    saveImage(image=image, name=output, format_=format_)
+
+    return True
+
--- main.py
+++ main.py
@@ -1,52 +1,5 @@
-from PIL import Image
-import numpy as np
 
 
-filin = open("input.txt", "r")
+from int2gray import process
 
-rows = int(filin.__iter__().__next__())
-cols = int(filin.__iter__().__next__())
-
-# create a 2 dimensional list using rows nd cols
-matrix = np.zeros((rows, cols))
-
-# fill in the double loop in order to fill the matrix
-for y in range(rows):
-    row = filin.__iter__().__next__()
-    row = row.rstrip(" ").lstrip(" ").split(" ")
-
-    for x in range(cols):
-        matrix[y, x] = int(row[x])
-
-# close the file 'input' file
-filin.close()
-
-# find the minimum and the maximum of the matrix below, store it inside the variable min
-min_, max_ = matrix[0, 0], matrix[0, 0]
-for y in range(rows):
-    
-    for x in range(cols):
-        
-        value = matrix[y, x]
-        
-        if value < min_:
-            min_ = value
-        if value > max_:
-            max_ = value
-        
-# compute the equation parameters
-a = float(255.0 / (max_-min_))
-b = -min_ * a
-
-# create a B&W image with the good size and load it
-new_image = Image.new('L', (cols, rows))
-image_pixels = new_image.load()
-
-# fill in the image pixel by pixel
-for y in range(len(matrix)):
-    for x in range(len(matrix[0])):
-        value = round( a * matrix[y, x] + b )
-        image_pixels[x, y] = min(255, max(0, value))
-
-# save output
-new_image.save('output.png', 'PNG')
+process(fileinput="input.txt", output="output", format_="png")
